460d42f860bbb9cdd25781f0603f7cfdfd4101bf
abdalrhman ak <abodasy2002213@gmail.com>
2022-08-16 01:25:54 +0300

    fixing some errors

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Listeners\newUserHasRegistered;
+use App\Listeners\NewUserHasRegistered;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
